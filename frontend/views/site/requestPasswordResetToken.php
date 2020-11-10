f01c0bc585e414bb79e470c0ba51be13eab94b55
Jim7 <Jim7rfp@gmail.com>
2020-11-10 23:14:56 +0300

    Небольшой причесон

--- frontend/views/site/requestPasswordResetToken.php
+++ frontend/views/site/requestPasswordResetToken.php
@@ -7,7 +7,7 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
-$this->title = 'Request password reset';
+$this->title = 'Восстановление пароля';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-request-password-reset">
